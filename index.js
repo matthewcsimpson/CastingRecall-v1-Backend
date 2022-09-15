8ef503359ca0d4aed90e9d0a709a493d4ed603d7
Matthew Simpson <mcsimpson+gh@gmail.com>
2022-09-15 13:24:45 -0700

    server.js renamed index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
 const app = express();
+const fs = require("fs");
 
 app.use(express.json());
 app.use(cors());
@@ -16,6 +17,29 @@ const TMDB_SEARCH_POP_URL = process.env.TMDB_SEARCH_POP_URL;
 const LOWEST_YEAR = 1990;
 const CURRENT_YEAR = new Date().getFullYear();
 
+/**
+ * Function to read the WAREHOUSE JSON file.
+ * @param {function} callback
+ */
+function loadData(callback) {
+  fs.readFile("./data/movies.json", "utf8", callback);
+}
+
+/**
+ * Function to overwrite the warehouses JSON file
+ * @param {stringify json} data
+ */
+function saveData(data) {
+  fs.writeFile(`./data/movies.json`, data, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+/**
+ * Function to generate a puzzle.
+ */
 const makePuzzle = () => {
   const randomYear = Math.floor(
     Math.random() * (CURRENT_YEAR - LOWEST_YEAR) + LOWEST_YEAR
@@ -24,6 +48,7 @@ const makePuzzle = () => {
   axios
     .get(`${TMDB_SEARCH_POP_URL}${randomYear}&api_key=${API_KEY}`)
     .then((res) => {
+      saveData(JSON.stringify(res.data.results));
       console.log(res.data.results);
     })
     .catch((e) => console.error(e));
